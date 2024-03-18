1a41bae817a1ea873cf28560a43ff9d8004add08
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-03-18 18:50:12 -0400

    Create My Trip Tab folder and TripMapView file

---
+++ MyMap/My Trip Tab/TripMapView.swift
@@ -0,0 +1,18 @@
+//
+//  TripMapView.swift
+//  MyMap
+//
+//  Created by Leonard McCook-Carr on 3/18/24.
+//
+
+import SwiftUI
+
+struct TripMapView: View {
+    var body: some View {
+        Text("Trip Map View!")
+    }
+}
+
+#Preview {
+    TripMapView()
+}
