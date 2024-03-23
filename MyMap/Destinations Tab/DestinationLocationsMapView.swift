77f00c4d05843f653d260e7b1ddb3673d977b970
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-03-23 14:18:38 -0400

    Import MapKit and set up map coordinates

--- MyMap/Destinations Tab/DestinationLocationsMapView.swift
+++ MyMap/Destinations Tab/DestinationLocationsMapView.swift
@@ -6,10 +6,21 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct DestinationLocationsMapView: View {
+    @State private var cameraPosition: MapCameraPosition =  .automatic
+    
     var body: some View {
-        Text("Destination Locations Map!")
+        Map(position: $cameraPosition)
+            .onAppear {
+                // 35.689506, 139.691700
+                
+                let tokyo = CLLocationCoordinate2D(latitude: 35.689506, longitude: 139.691700)
+                let tokyoSpan = MKCoordinateSpan(latitudeDelta: 0.25, longitudeDelta: 0.25)
+                let tokyoRegion = MKCoordinateRegion(center: tokyo, span: tokyoSpan)
+                cameraPosition = .region(tokyoRegion)
+            }
     }
 }
 
