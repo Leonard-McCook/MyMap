da43292d38fbeb437b548ef4e79bfbe1e271b455
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-03-25 18:45:24 -0400

    Add more marker initializers

--- MyMap/Destinations Tab/DestinationLocationsMapView.swift
+++ MyMap/Destinations Tab/DestinationLocationsMapView.swift
@@ -14,7 +14,19 @@ struct DestinationLocationsMapView: View {
     var body: some View {
         Map(position: $cameraPosition) {
             Marker("Moulin Rouge", coordinate: .moulinRouge)
+            Marker(coordinate: .arcDeTriomphe) {
+                Label("Arch de Triomphe", systemImage: "star.fill")
+            }
+            .tint(.yellow)
+            Marker("Eiffel Tower", image: "eiffelTower", coordinate: .eiffelTower)
+                .tint(.blue)
+            Marker("Gare du Nord", monogram: Text("GN"), coordinate: .gareDuNord)
+                .tint(.accent)
+            
+            Marker("Louvre", systemImage: "person.crop.artframe", coordinate: .louvre)
+                .tint(.appBlue)
         }
+        
             .onAppear {
                 let paris = CLLocationCoordinate2D(latitude: 48.856788, longitude: 2.351077)
                 let parisSpan = MKCoordinateSpan(latitudeDelta: 0.15, longitudeDelta: 0.15)
