794e65cde4106e5ecf0190ef291e2047034ff225
Leonard-McCook <leonard.mccookcarr@outlook.com>
2024-03-30 14:15:28 -0400

    Create Destination class

--- MyMap/Models/Destination.swift
+++ MyMap/Models/Destination.swift
@@ -5,4 +5,35 @@
 //  Created by Leonard McCook-Carr on 3/29/24.
 //
 
-import Foundation
+import SwiftData
+import MapKit
+
+@Model
+class Destination {
+    var name: String
+    var latitude: Double?
+    var longitude: Double?
+    var latitudeDelta: Double?
+    var longitudeDelta: Double?
+    @Relationship(deleteRule: .cascade)
+   
+    
+    init(name: String, latitude: Double? = nil, longitude: Double? = nil, latitudeDelta: Double? = nil, longitudeDelta: Double? = nil) {
+        self.name = name
+        self.latitude = latitude
+        self.longitude = longitude
+        self.latitudeDelta = latitudeDelta
+        self.longitudeDelta = longitudeDelta
+    }
+    
+    var region: MKCoordinateRegion? {
+        if let latitude, let longitude, let latitudeDelta, let longitudeDelta {
+            return MKCoordinateRegion(
+                center: CLLocationCoordinate2D(latitude: latitude, longitude: longitude),
+                span: MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+            )
+        } else {
+            return nil
+        }
+    }
+}
